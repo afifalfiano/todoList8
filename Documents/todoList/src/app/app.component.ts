58f15da398532a8da765c55cf6487b2f8f94a35e
Afif Alfiano <afif.alfiano@uii.ac.id>
2020-07-09 21:25:50 +0700

    add moment and ngx-bootstrap modal

--- Documents/todoList/src/app/app.component.ts
+++ Documents/todoList/src/app/app.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import * as moment from 'moment';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'todolist';
   todos;
   addTodo;
   todoList;
+  Time;
+  modalRef: BsModalRef;
 
-  constructor() {
+
+  constructor(
+    private modalSvc: BsModalService
+  ) {
     this.addTodo = '';
     this.todos = [];
 
   }
 
+  ngOnInit() {
+    this.Time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  }
+
+  showModal(template) {
+    this.modalRef = this.modalSvc.show(template);
+
+  }
   todoSubmit(event) {
     this.todoList = {
       addTodo: this.addTodo,
--- Documents/todoList/src/app/app.module.ts
+++ Documents/todoList/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule, BsModalRef } from 'ngx-bootstrap/modal';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +14,10 @@ import { BrowserModule } from '@angular/platform-browser';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [BsModalRef],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
