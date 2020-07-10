954e59184a5ad1e5849b2f877ead70a2c9de00be
afif.alfiano <afif.alfiano@uii.ac.id>
2020-07-10 14:06:51 +0700

    angular 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'todoList8';
+export class AppComponent implements OnInit {
+  title = 'todolist';
+  todos;
+  addTodo;
+  todoList;
+  Time;
+  modalRef: BsModalRef;
+
+
+  constructor(
+    private modalSvc: BsModalService
+  ) {
+    this.addTodo = '';
+    this.todos = [];
+
+  }
+
+  ngOnInit() {
+    this.Time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  }
+
+  showModal(template) {
+    this.modalRef = this.modalSvc.show(template);
+
+  }
+  todoSubmit(event) {
+    this.todoList = {
+      addTodo: this.addTodo,
+    };
+    this.todos.push(this.todoList.addTodo);
+    this.addTodo = '';
+    // console.log(this.todos);
+    event.preventDefault();
+
+  }
+
+  deleteTodo(index) {
+    this.todos.splice(index, 1);
+  }
+
+  deleteAll(index) {
+    const todoLength = this.todos.length;
+    this.todos.splice(index, todoLength);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalModule, BsModalRef } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,9 +12,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [BsModalRef],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
