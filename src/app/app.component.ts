4c38b4e7276f574a42a29e421181bc3c0ae68a1d
afifalfiano <afifalfiano2@gmail.com>
2020-07-18 15:51:33 +0700

    fresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as moment from 'moment';
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
@@ -9,46 +7,11 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 })
 export class AppComponent implements OnInit {
   title = 'todolist';
-  todos;
-  addTodo;
-  todoList;
-  Time;
-  modalRef: BsModalRef;
-
-
   constructor(
-    private modalSvc: BsModalService
   ) {
-    this.addTodo = '';
-    this.todos = [];
 
   }
 
   ngOnInit() {
-    this.Time = moment().format('MMMM Do YYYY, h:mm:ss a');
-  }
-
-  showModal(template) {
-    this.modalRef = this.modalSvc.show(template);
-
-  }
-  todoSubmit(event) {
-    this.todoList = {
-      addTodo: this.addTodo,
-    };
-    this.todos.push(this.todoList.addTodo);
-    this.addTodo = '';
-    // console.log(this.todos);
-    event.preventDefault();
-
-  }
-
-  deleteTodo(index) {
-    this.todos.splice(index, 1);
-  }
-
-  deleteAll(index) {
-    const todoLength = this.todos.length;
-    this.todos.splice(index, todoLength);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { ModalModule, BsModalRef } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -14,9 +13,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    ModalModule.forRoot()
   ],
-  providers: [BsModalRef],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
